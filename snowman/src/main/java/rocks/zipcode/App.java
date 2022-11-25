84a106da7afcbe0a0dd4ded753fcc4a5fdf0b7d0
thienle77 <tle9677@gmail.com>
2022-11-24 22:45:42 -0500

    completed

--- snowman/src/main/java/rocks/zipcode/App.java
+++ snowman/src/main/java/rocks/zipcode/App.java
@@ -14,4 +14,5 @@ public class App
 
         System.out.println(output);
     }
+
 }
--- snowman/src/test/java/rocks/zipcode/AppTest.java
+++ snowman/src/test/java/rocks/zipcode/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.sql.SQLOutput;
+
 /**
  * Unit test for simple App.
  */
@@ -15,6 +17,15 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+        String input = "(print (multiply 2 (subtract 6 3)))";
+
+        Compiler compiler = new Compiler();
+
+        System.out.println(";; Input code: " +input);
+
+        String output = compiler.compile(input);
+
+        System.out.println(output);
         assertTrue( true );
     }
 }
